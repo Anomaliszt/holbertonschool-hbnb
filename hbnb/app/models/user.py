535d537fbde2442e34f5c5e842e89fb48fe7c570
Anomaliszt <xavier.sanchez44@orange.fr>
2024-10-23 10:47:11 +0200

    test

--- hbnb/app/models/user.py
+++ hbnb/app/models/user.py
@@ -2,18 +2,18 @@ import re
 from app.models.base_model import BaseModel
 
 class User(BaseModel):
+    EMAIL_REGEX = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+
     def __init__(self, first_name, last_name, email, is_admin=False):
         super().__init__()
 
-        EMAIL_REGEX = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
-
-        if not first_name or len(first_name) > 50:
+        if not isinstance(first_name, str) or len(first_name) > 50:
             raise ValueError("First name is required and must be 50 characters or less")
-        
-        if not last_name or len(last_name) > 50:
+
+        if not isinstance(last_name, str) or len(last_name) > 50:
             raise ValueError("Last name is required and must be 50 characters or less")
-        
-        if not email or not re.match(self.EMAIL_REGEX, email):
+
+        if not email or not isinstance(email, str) or not re.match(User.EMAIL_REGEX, email):
             raise ValueError("A valid email address is required")
 
         self.first_name = first_name
